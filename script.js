ff1adb7b00c631172057348c5e149e58e6e4b8d6
AStandish <112442942+AStandish@users.noreply.github.com>
2022-11-03 19:21:11 -0400

    added api link

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-//l
+function getApi() {
+   
+    const requestUrl ="http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={a74bf83c5240b68543e1753ef9d16b93}";
