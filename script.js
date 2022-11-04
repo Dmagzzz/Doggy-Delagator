82e06ff71d689521f22450111e26784e3bc7ac66
Dillon Maglio <hddod15@gmail.com>
2022-11-03 21:15:27 -0400

    added bulma

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Assignment code here
+
+function setup() {
+    createCanvas(200, 200);
+    loadJSON ('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all', gotData):
+    }
+    
+    function gotData(data) {
+        println(data);
+    }
+
+    function draw() {
+        backround(0);
+    }
