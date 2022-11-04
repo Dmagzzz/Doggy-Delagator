3171f71133df5d7d9ca695187c02659a6ca41dd5
AStandish <112442942+AStandish@users.noreply.github.com>
2022-11-03 20:59:17 -0400

    added the openweather and fetch

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 function getApi() {
-   
-    const requestUrl ="http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={a74bf83c5240b68543e1753ef9d16b93}";
+  const requestUrl =
+    "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={9d69570cc2097eb0470c7256980f5753"}
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      //Loop over the data to generate a table, each table row will have a link to the repo url
+      for (var i = 0; i < data.length; i++) {
+        // Creating elements, tablerow, tabledata, and anchor
+        var createTableRow = document.createElement("tr");
+        var tableData = document.createElement("td");
+        var link = document.createElement("a");
+
+        // Setting the text of link and the href of the link
+        link.textContent = data[i].html_url;
+        link.href = data[i].html_url;
+
+        // Appending the link to the tabledata and then appending the tabledata to the tablerow
+        // The tablerow then gets appended to the tablebody
+        tableData.appendChild(link);
+        createTableRow.appendChild(tableData);
+        tableBody.appendChild(createTableRow);
+      }
+    });
+}
+
+fetchButton.addEventListener("click", getApi);
