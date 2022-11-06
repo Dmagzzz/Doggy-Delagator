9ccd7fb5b32d775931622fe71f6399f57881926d
AStandish <112442942+AStandish@users.noreply.github.com>
2022-11-06 14:03:22 -0500

    updated url and added correct function

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-function getApi() {
-  const requestUrl =
-    "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={9d69570cc2097eb0470c7256980f5753"}
+function getApi(input) {
+  // fetch request gets a list of all the repos for the node.js organization
+  const requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=a74bf83c5240b68543e1753ef9d16b93`;
+
   fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      getWeather(data[0]);
+    });
+}
+
+function getWeather(location) {
+  const lat = location.lat;
+  const lon = location.lon;
+  const requestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=a74bf83c5240b68543e1753ef9d16b93`;
+  fetch(requestUrl)
+    .then(function (res) {
+      return res.json();
     })
     .then(function (data) {
       console.log(data);
-      //Loop over the data to generate a table, each table row will have a link to the repo url
-      for (var i = 0; i < data.length; i++) {
-        // Creating elements, tablerow, tabledata, and anchor
-        var createTableRow = document.createElement("tr");
-        var tableData = document.createElement("td");
-        var link = document.createElement("a");
-
-        // Setting the text of link and the href of the link
-        link.textContent = data[i].html_url;
-        link.href = data[i].html_url;
-
-        // Appending the link to the tabledata and then appending the tabledata to the tablerow
-        // The tablerow then gets appended to the tablebody
-        tableData.appendChild(link);
-        createTableRow.appendChild(tableData);
-        tableBody.appendChild(createTableRow);
-      }
     });
 }
+
+getApi("Exeter");
 
 fetchButton.addEventListener("click", getApi);
