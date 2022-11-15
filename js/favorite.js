62772a75a93cdf759c962136c726ea7a3b6da764
AStandish <112442942+AStandish@users.noreply.github.com>
2022-11-15 18:41:30 -0500

    deleted old code

--- js/favorite.js
+++ js/favorite.js
@@ -1,12 +1,8 @@
-// This
 const history = JSON.parse(localStorage.getItem("savedFavoritePhotos"));
 let fullTemplate = "";
 
 for (let i = 0; i < history.length; i++) {
-  // const allPhotos = document.createElement("img");
-  // allPhotos.setAttribute("src", history[i]);
   fullTemplate += renderRow(history[i]);
-  //document.getElementById("allPhotos").appendChild(allPhotos);
 }
 function renderRow(imgUrl) {
   const htmlTemplate = `<div class="columns"><div class="column"><img src="${imgUrl}"/></div></div>`;
